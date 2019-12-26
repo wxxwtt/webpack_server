1fc55e1405574f5ffb2545830b2b422f6e30e835
Tom <weixinxiu@digilinx.cn>
2019-12-26 16:39:12 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,34 +1,10 @@
-// import * as foo from './foo'
-// import './main.css'
-// console.log(foo);
-// console.log('o-o');
-$('#list').on('click', '.add', function (e) {
-  e.stopPropagation()
-  var liHtml = `
-                <li>
-                  开始: <input type="text" class="starttime"> 结束:<input type="text" class="endtime"><button class="del">删除</button> <button class="add">添加</button>
-                </li>
-              `
-  $('#list').append(liHtml)
+import Foo from './foo'
+console.log(Foo);
+
+// 加载第三方模块
+import $ from 'jquery'
+$('#app').css({
+  width:200,
+  height:200,
+  background:'pink'
 })
-$('#list').on('input','input',function(){
-  var starttime = $(this).val()
-  console.log(starttime);
-})
-
-$('#list').on('click', '.del', function (e) {
-
-  e.stopPropagation()
-  $(this).parent('li').remove()
-
-
-})
-var fn = () => {
-
-}
-console.log(fn);
-const arr = [{id:1},{id:2},{id:3}]
-var res = arr.filter(item => {
-  return item.id == 2
-})
-console.log(res);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-  entry: ["@babel/polyfill", "./src/main.js"],
+  entry: ["@babel/polyfill", "./src/main.js"], //@babel/polyfill es6api兼容
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -13,7 +13,10 @@ module.exports = {
     })
   ],
   devServer: {
-    contentBase: './dist'
+    contentBase: './'
+  },
+  externals: {
+    jquery: 'jQuery' //第三方包 不打包进main的js中 减少文件体积
   },
   module: {
     rules: [
@@ -38,7 +41,7 @@ module.exports = {
           options: {
             cacheDirectory: true,// 开启打包编译缓存 提高利率
             presets: ['@babel/preset-env'],// 转es5
-            plugins: ["@babel/plugin-transform-runtime"] //去除重复的代码
+            plugins: ["@babel/plugin-transform-runtime"] //去除重复辅助的方法
           }
         }
       }
